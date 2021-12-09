013b1e90b5f1ec1cdf559c5b67f2182ce5969674
Carlos Mario Cárdenas <90805618+CarlosCardenas94@users.noreply.github.com>
2021-12-09 12:35:23 -0500

    Plantilla

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import inicio from './assets/inicio.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Navbar />
+        <div>
+          <img src={inicio}/>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
