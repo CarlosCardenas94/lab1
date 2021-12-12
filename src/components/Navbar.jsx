3026d24d354f4f8873f09fd046405565e1db7e58
douglasramosg <douglasr@uninorte.edu.co>
2021-12-12 16:27:49 -0500

    retorno htnml

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,57 +2,32 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        React.createElement("nav", {
-            class: "navbar navbar-expand-lg navbar-light bg-light"
-          }, /*#__PURE__*/React.createElement("div", {
-            class: "container-fluid"
-          }, /*#__PURE__*/React.createElement("a", {
-            class: "navbar-brand",
-            href: "#"
-          }, "T-LAB"), /*#__PURE__*/React.createElement("button", {
-            class: "navbar-toggler",
-            type: "button",
-            "data-bs-toggle": "collapse",
-            "data-bs-target": "#navbarNavDropdown",
-            "aria-controls": "navbarNavDropdown",
-            "aria-expanded": "false",
-            "aria-label": "Toggle navigation"
-          }, /*#__PURE__*/React.createElement("span", {
-            class: "navbar-toggler-icon"
-          })), /*#__PURE__*/React.createElement("div", {
-            class: "collapse navbar-collapse",
-            id: "navbarNavDropdown"
-          }, /*#__PURE__*/React.createElement("ul", {
-            class: "navbar-nav"
-          }, /*#__PURE__*/React.createElement("li", {
-            class: "nav-item"
-          }, /*#__PURE__*/React.createElement("a", {
-            class: "nav-link active",
-            "aria-current": "page",
-            href: "#"
-          }, "Inicio")), /*#__PURE__*/React.createElement("li", {
-            class: "nav-item"
-          }, /*#__PURE__*/React.createElement("a", {
-            class: "nav-link",
-            href: "#"
-          }, "Precios")), /*#__PURE__*/React.createElement("li", {
-            class: "nav-item dropdown"
-          }, /*#__PURE__*/React.createElement("a", {
-            class: "nav-link dropdown-toggle",
-            href: "#",
-            id: "navbarDropdownMenuLink",
-            role: "button",
-            "data-bs-toggle": "dropdown",
-            "aria-expanded": "false"
-          }, "Atenticaci\xF3n"), /*#__PURE__*/React.createElement("ul", {
-            class: "dropdown-menu",
-            "aria-labelledby": "navbarDropdownMenuLink"
-          }, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-            class: "dropdown-item",
-            href: "#"
-          }, "Iniciar sesi\xF3n")), /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("a", {
-            class: "dropdown-item",
-            href: "#"
-          }, "Registrarse"))))))))
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">T-LAB</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+              <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#">Precios</a>
+              </li>
+              <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Atenticación
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item" href="#">Iniciar sesión</a></li>
+                  <li><a class="dropdown-item" href="#">Registrarse</a></li>
+              </ul>
+              </li>
+          </ul>
+          </div>
+      </div>
+  </nav>
     )
 }
